47f203db21aba06a178ba96915d8e4236bc5e4eb
Peerapat siriai <peecorei1@gmail.com>
2024-04-03 09:09:39 +0700

    group domain

--- internal/domain/book_domain.go
+++ internal/domain/book_domain.go
@@ -4,6 +4,14 @@ import (
 	entities "go/api/internal/entities"
 )
 
+type BookUseCase interface {
+	CreateBook(book *entities.Book) error
+	UpdateBook(id uint, book *entities.Book) error
+	DeleteBook(id uint) error
+	GetBookByID(id uint) (*entities.Book, error)
+	GetAllBooks() ([]*entities.Book, error)
+}
+
 type BookRepository interface {
 	Create(book *entities.Book) error
 	Update(book *entities.Book) error
